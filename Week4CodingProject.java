6b57dffa87735a60658aa38c652064b00d959879
Jesus guerra <guerrajesus@live.com>
2024-12-07 17:32:34 -0800

    week 4 project

--- Week4CodingProject.java
+++ Week4CodingProject.java
@@ -14,7 +14,7 @@ public class Week4CodingProject {
 		
 		//1.a 
 		//firstSubLast = (ages[ages.length-1]) - ages[0];
-		System.out.print("1.a 1st - last = ");
+		System.out.print("1.a) 1st - last = ");
 		printingfirstSubLast(ages);
 
 		findingAverge(ages);
@@ -22,7 +22,7 @@ public class Week4CodingProject {
 		//1.b 		
 
 		int [] ages2 = {3, 9, 23, 64, 2, 8, 28, 93, 7};
-		System.out.print("1.ii = ");
+		System.out.print("1.ii) ");
 		printingfirstSubLast(ages2);
 		
 		//2 
@@ -92,7 +92,7 @@ public class Week4CodingProject {
 			System.out.print("13) ");
 			System.out.println(String.format("%.2f",totalWithTaxes(restaurantBill, eachTipAmount)));
 			
-			
+			//"%.2f" to print double in a 2 decimal format 
 			
 			
 	// method to help figure out your part of the bill when going out with friends. 
@@ -115,23 +115,23 @@ public class Week4CodingProject {
 	public static boolean willBuyDrink(boolean isHotOutside, double moneyInPocket) {
 		double money = 10.50;
 		
-		if (isHotOutside && (moneyInPocket > money)) {
+		if (isHotOutside && (moneyInPocket > money)) {   // if both represented by && are true then return true
 			return true;
 		} else {
 			
-		} return false;
+		} return false;   // if 1 is false then whole thing is false 
 			
 	}
 	//11
 	public static boolean firstArrayIsGreater (double [] array1, double [] array2) {
-		double sumArray1=0, sumArray2 =0;
+		double sumArray1=0, sumArray2 =0;   
 			for (double sum1 : array1) {
-				sumArray1 += sum1;
+				sumArray1 += sum1;          
 			} for (double sum2 : array2) {
-				sumArray2 += sum2;
-			} if ((sumArray1/array1.length) > (sumArray2/array2.length)) {
-					return true;
-			}else {
+				sumArray2 += sum2;          // these 2 above are the same concept as #10
+			} if ((sumArray1/array1.length) > (sumArray2/array2.length)) { // takes both sums from array1 and 2 to compare if the first one is larger in average.
+					return true;              // did the average in the paragraph above and compared the 2             
+			}else {                           // returning true if 1st array is large or false if 2nd is large 
 					return false;
 		}		
 		
@@ -144,25 +144,25 @@ public class Week4CodingProject {
 		double sum =0.00;
 		for (double num : arrays) {
 			sum += num;
-		} return sum/arrays.length;
+		} return sum/arrays.length;  //at the end get the total added up in sum and divide by length which is how many numbers were in the array to get average 
 	}
 	
 	//9
 	public static boolean greaterThan100 (int[] nums) {
 		int result =0;
-		for(int num : nums) {
-			result += num;
-		} if (result > 100) {
+		for(int num : nums) {   //enhance for loop again to get all the elements in array
+			result += num;      // same thing adding them all up and results holds value
+		} if (result > 100) {   // after adding it if result is more than 100 then its true
 			
 		} return true;
 	}
 	
 	//8
 	public static String fullName(String firstName, String lastName) {
-		StringBuilder fullName = new StringBuilder();
-		fullName.append(firstName + " ");
-		fullName.append(lastName);
-		return fullName.toString();
+		StringBuilder fullName = new StringBuilder();  //now that we know StringBuilder found it easier that in #7 with a regular String but similar idea
+		fullName.append(firstName + " ");      //adds firstName and a space in " " to fullName String
+		fullName.append(lastName);			   // addes the lastName
+		return fullName.toString();			   // groups it all together into a String to return 
 	}
 	
 	
@@ -170,7 +170,7 @@ public class Week4CodingProject {
 	public static String returningConcat(String word, int n) {
 		String concat ="";
 		for (int i =0; i <n; i++) {
-			concat += word;
+			concat += word;  // similar to 1.c below concat hold the new String in concat and += keeps adding the word n number of times 
 		} return concat;
 	}
 	
@@ -183,9 +183,9 @@ public class Week4CodingProject {
 	//1. c
 	public static void findingAverge (int[] ages) { 
 		int results =0;
-		for(int age : ages) {
-			results += age;
-		} System.out.println("1.c average " + results/ages.length);
+		for(int age : ages) {   //enhance for loop to get all age in ages
+			results += age;     // results hold the value and addes them all up
+		} System.out.println("1.c) average " + results/ages.length);
 	} 
 
 }
